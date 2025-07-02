1b1316a63d1d689b1795ee57aea5b38e963d4a85
tasha maina <tashabanks271@gmail.com>
2025-07-02 20:51:30 +0300

    Completed domcontentloaded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+
+console.log("This logs immediately when index.js is loaded.");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded.");
+  const paragraph = document.getElementById("text");
+  paragraph.textContent = "This is really cool!";
+});
